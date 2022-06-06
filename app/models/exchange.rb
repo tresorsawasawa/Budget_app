f2f2faf5c7f7b904cc14daea12e3240b10703f4e
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-06 16:10:30 +0200

    Add validations and generate the schema

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -2,4 +2,6 @@ class Exchange < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :slots
   has_many :categories, through: :slots
+
+  validates_presence_of :name, :amount, :author
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :categories
   has_many :exchanges, foreign_key: 'author_id'
+
+  validates :nawe, presence: true, length: { maximum: 50 }
 end
