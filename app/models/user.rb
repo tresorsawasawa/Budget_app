1c6ece2d64958af9a76a5db61d1093c98fc791ae
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-07 22:25:31 +0200

    Add units tests on the Category model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :categories
   has_many :exchanges, foreign_key: 'author_id'
 
-  validates :nawe, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before (:each) do
+    @tresor = User.create(name: 'Tresor', email: 'tresor@test.com', password: 'password')
+    expect(@tresor).to be_valid
+    @clothes_category = Category.create(name: 'Clothes', icon: 'C', user_id: @tresor.id)
+  end
+
+  after(:each) do
+    User.destroy_all
+    Category.destroy_all
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@clothes_category).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    @clothes_category.name = nil
+    expect(@clothes_category).to_not be_valid
+  end
+
+  it 'is not valid without an icon' do
+    @clothes_category.icon = nil
+    expect(@clothes_category).to_not be_valid
+  end
+
+  it 'is not valid if name length is more than 30 characters' do
+    @clothes_category.name = 'a' * 40
+    expect(@clothes_category).to_not be_valid
+  end
+
+  it 'is not valid if icon length is more than 20 characters' do
+    @clothes_category.icon = 'i' * 40
+    expect(@clothes_category).to_not be_valid
+  end
+
+  describe 'Associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:exchanges).through(:slots) }
+  end
 end
