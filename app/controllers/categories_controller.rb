363ce96f3677126e809c46c830a1641073e6f0c3
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-08 15:50:09 +0200

    Implement the categories controler with all methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,49 +1,62 @@
 class CategoriesController < ApplicationController
-  before_action :set_exchanges, only: %i[show edit update destroy]
+  before_action :set_category, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
   def index
-    @exchanges = Exchange.all
+    @categories = Category.all
   end
+
+  def show; end
 
   def new
-    @exchange = Exchange.new
+    @category = Category.new
   end
 
+  def edit; end
+  
   def create
-    @exchange = Exchange.new(exchange_params)
-    @exchange.author = current_user
+    @category = Category.new(category_params)
+    @category.author = current_user
 
     respond_to do
-      if @exchange.save
-        format.html { redirect_to exhange_path(@exchange), notice: 'Exchange was successfully created.' }
-        format.json { render :show, status: :created, location: @exchange }
+      if @category.save
+        format.html { redirect_to category_path(@category), notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @exchange.errors, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
     respond_to do |format|
-      if @exchange.update(exchange_params)
-        format.html { redirect_to exchange_url(@exchange), notice: 'Exchange was successfully updated.' }
-        format.json { render :show, status: :ok, location: @exchange }
+      if @category.update(category_params)
+        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @exchange.errors, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def destroy
+    @category.destroy
+
+    respond_to do |format|
+      format.html { redirect_to categories_url, 'Category was successfully deleted.' }
+      format.json { head :no_content }
     end
   end
 
   private
 
-  def set_exchanges
-    @exchange = Exchange.find(params[:id])
+  def set_category
+    @category = Category.find(params[:id])
   end
 
-  def exchange_params
-    params.require(:exchange).permit(:name, :amount)
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
