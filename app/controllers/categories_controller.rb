697cbe7156f4804112f81346aff9ae5c95facbd0
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-08 02:30:43 +0200

    Add logic in exchanges controler

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,49 @@
 class CategoriesController < ApplicationController
+  before_action :set_exchanges, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+
+  def index
+    @exchanges = Exchange.all
+  end
+
+  def new
+    @exchange = Exchange.new
+  end
+
+  def create
+    @exchange = Exchange.new(exchange_params)
+    @exchange.author = current_user
+
+    respond_to do
+      if @exchange.save
+        format.html { redirect_to exhange_path(@exchange), notice: 'Exchange was successfully created.' }
+        format.json { render :show, status: :created, location: @exchange }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @exchange.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @exchange.update(exchange_params)
+        format.html { redirect_to exchange_url(@exchange), notice: 'Exchange was successfully updated.' }
+        format.json { render :show, status: :ok, location: @exchange }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @exchange.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_exchanges
+    @exchange = Exchange.find(params[:id])
+  end
+
+  def exchange_params
+    params.require(:exchange).permit(:name, :amount)
+  end
 end
