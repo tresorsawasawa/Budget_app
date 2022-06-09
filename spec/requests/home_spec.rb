3220aa34afd96564b1f63d8db255483b13d17045
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-09 10:35:33 +0200

    Add unit tests on home page

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,11 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Homes', type: :request do
-#   describe 'GET /home' do
-#     it 'returns http success' do
-#       get '/home/home'
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
+RSpec.describe 'Homes', type: :request do
+  describe 'GET /home' do
+    it 'returns http success' do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
+end
