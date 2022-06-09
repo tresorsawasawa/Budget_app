e33a615513e69d054d3e4d5d0f0631a9cd1296d5
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-09 05:49:16 +0200

    Fix a bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # get 'home/home'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
